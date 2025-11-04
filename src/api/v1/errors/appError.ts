44b7bc0079bfb36841aed2d5b6622a5e97ce6294
jasleen kaur <jjasleenkaur2@rrc.ca>
2025-11-04 00:30:29 -0600

    wrote the code in the apperror file to identify the error in the app

--- src/api/v1/errors/appError.ts
+++ src/api/v1/errors/appError.ts
@@ -1,1 +1,14 @@
+import { HttpStatusCode } from "../constants/httpstatuscodes";
 
+export class AppError extends Error {
+  public readonly statusCode: HttpStatusCode;
+  public readonly isOperational: boolean;
+
+  constructor(message: string, statusCode: HttpStatusCode, isOperational = true) {
+    super(message);
+    this.statusCode = statusCode;
+    this.isOperational = isOperational;
+
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
