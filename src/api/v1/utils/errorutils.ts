af7cf0b780e6717560a8c68c5f4679fee55f7fe7
jasleen kaur <jjasleenkaur2@rrc.ca>
2025-11-04 00:40:45 -0600

    Created the utility functions to handle error extraction consistently.

--- src/api/v1/utils/errorutils.ts
+++ src/api/v1/utils/errorutils.ts
@@ -1,1 +1,23 @@
+import { AppError } from "../errors/appError";
+import { HTTP_STATUS } from "../constants/httpstatuscodes";
 
+export const formatErrorResponse = (error: unknown) => {
+  const timestamp = new Date().toISOString();
+
+  if (error instanceof AppError) {
+    return {
+      success: false,
+      message: error.message,
+      statusCode: error.statusCode,
+      timestamp,
+    };
+  }
+
+  // For unexpected errors
+  return {
+    success: false,
+    message: "Internal Server Error",
+    statusCode: HTTP_STATUS.INTERNAL_SERVER_ERROR,
+    timestamp,
+  };
+};
