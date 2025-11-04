b11c1c845632a0a49b04ad1f62546ce52e582b9f
jasleen kaur <jjasleenkaur2@rrc.ca>
2025-11-04 00:35:21 -0600

    wrote the code in the notfounderror file to identify the error

--- src/api/v1/errors/notfoundError.ts
+++ src/api/v1/errors/notfoundError.ts
@@ -1,1 +1,8 @@
+import { AppError } from "./appError";
+import { HTTP_STATUS } from "../constants/httpstatuscodes";
 
+export class NotFoundError extends AppError {
+  constructor(message = "Resource Not Found") {
+    super(message, HTTP_STATUS.NOT_FOUND);
+  }
+}
